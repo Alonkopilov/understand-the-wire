c46f1965a96c50520daceb8df69383bb9226f277
Alon Kopilov <alonk@altshare.com>
2026-07-23 15:43:46 +0300

    Updated health check to alb target group

--- infra/ec2/load-balancers.tf
+++ infra/ec2/load-balancers.tf
@@ -12,7 +12,14 @@ resource "aws_lb_target_group" "private_instance_tg" {
   name     = "private-instance-tg"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = var.vpc_id
+
+  vpc_id = var.vpc_id
+
+  health_check {
+    path                = "/health"
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+  }
 }
 
 resource "aws_lb_target_group_attachment" "alb_tg" {
